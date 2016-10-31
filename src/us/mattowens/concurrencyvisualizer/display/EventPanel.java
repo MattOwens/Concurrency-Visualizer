6b7f7dd1f83ca30e2151599c2b14d10e359c2083
Matthew <Matthew@Owens>
2016-10-31 10:18:11 -0400

    Before I make the switch to internal frames

--- src/us/mattowens/concurrencyvisualizer/display/EventPanel.java
+++ src/us/mattowens/concurrencyvisualizer/display/EventPanel.java
@@ -1,5 +1,8 @@
 package us.mattowens.concurrencyvisualizer.display;
 
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -12,10 +15,10 @@ public class EventPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private ArrayList<JLabel> dataLabels;
+
+	
 	
 	public EventPanel(DisplayEvent event) {
-		dataLabels = new ArrayList<JLabel>();
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		addNewLabel("Event Class", event.getEventClass());
 		addNewLabel("Event Type", event.getEventType());
@@ -25,24 +28,23 @@ public class EventPanel extends JPanel {
 		addNewLabel("Thread Id", event.getThreadId());
 		addNewLabel("Join Point", event.getJoinPointName());
 		
-		
 		for(Object pair : event.getEventMap().entrySet()) {
 			@SuppressWarnings("unchecked")
 			Map.Entry<String, Object> mapEntry = (Map.Entry<String, Object>) pair;
 			addNewLabel(mapEntry.getKey(), mapEntry.getValue());
 		}
-		
-		
+		//Force panel to recalculate the necessary size
+		setMaximumSize(getPreferredSize());
+		setMinimumSize(getPreferredSize());
 	}
 	
 	private void addNewLabel(String key, Object value) {
 		if(value == null) {
 			System.out.println("Null pointer in addNewLabel : " + key);
 		}
-		else {
-			JLabel newLabel = new JLabel(key + ": " + value.toString());
-			dataLabels.add(newLabel);
-			this.add(newLabel);
+		else {			
+			JLabel fieldLabel = new JLabel(key + ": " + value.toString());
+			this.add(fieldLabel);
 		}
 	}
 
--- src/us/mattowens/concurrencyvisualizer/display/ThreadDisplayPanel.java
+++ src/us/mattowens/concurrencyvisualizer/display/ThreadDisplayPanel.java
@@ -1,5 +1,7 @@
 package us.mattowens.concurrencyvisualizer.display;
 
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 public class ThreadDisplayPanel extends JPanel {
